563ad415d17f38e0737094349d19078914ace3e4
BOOBATHITHILLAN <boobathithillan@gmail.com>
2023-05-29 17:23:25 +0530

    update

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let countdownElement = document.getElementById("countdown");    
-    
-    setTimeout(()=> {
-      countdownElement.innerText = "10";
-      countdownElement.className="chakra size"
-      setTimeout(()=> {
-        countdownElement.innerText = "9";
-        countdownElement.className="chakra size"
-        setTimeout(()=> {
-          countdownElement.innerText = "8";
-          countdownElement.className="chakra size"
-          setTimeout( ()=> {
-            countdownElement.innerText = "7";
-            countdownElement.className="chakra size"
-            setTimeout(()=> {
-              countdownElement.innerText = "6";
-              countdownElement.className="chakra size"
-              setTimeout(()=> {
-                countdownElement.innerText = "5";
-                countdownElement.className="chakra size"
-                setTimeout(()=> {
-                  countdownElement.innerText = "4";
-                  countdownElement.className="chakra size"
-                  setTimeout(()=> {
-                    countdownElement.innerText = "3";
-                    countdownElement.className="chakra size"
-                    setTimeout(()=> {
-                      countdownElement.innerText = "2";
-                      countdownElement.className="chakra size"
-                      setTimeout( ()=> {
-                        countdownElement.innerText = "1";
-                        countdownElement.className="chakra size"
-                        setTimeout( ()=> {
-                          countdownElement.innerText = "";
-                          document.getElementById("flag").style.opacity = "1";
-                        }, 1000);
+  let countdownElement = document.getElementById("countdown");
+
+  setTimeout(() => {
+    countdownElement.innerText = "10";
+    countdownElement.className = "chakra size"
+    setTimeout(() => {
+      countdownElement.innerText = "9";
+      countdownElement.className = "chakra size"
+      setTimeout(() => {
+        countdownElement.innerText = "8";
+        countdownElement.className = "chakra size"
+        setTimeout(() => {
+          countdownElement.innerText = "7";
+          countdownElement.className = "chakra size"
+          setTimeout(() => {
+            countdownElement.innerText = "6";
+            countdownElement.className = "chakra size"
+            setTimeout(() => {
+              countdownElement.innerText = "5";
+              countdownElement.className = "chakra size"
+              setTimeout(() => {
+                countdownElement.innerText = "4";
+                countdownElement.className = "chakra size"
+                setTimeout(() => {
+                  countdownElement.innerText = "3";
+                  countdownElement.className = "chakra size"
+                  setTimeout(() => {
+                    countdownElement.innerText = "2";
+                    countdownElement.className = "chakra size"
+                    setTimeout(() => {
+                      countdownElement.innerText = "1";
+                      countdownElement.className = "chakra size"
+                      setTimeout(() => {
+                        countdownElement.innerText = "";
+                        document.getElementById("flag").style.opacity = "1";
                       }, 1000);
                     }, 1000);
                   }, 1000);
@@ -45,5 +44,5 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 1000);
       }, 1000);
     }, 1000);
+  }, 1000);
 });
-  
